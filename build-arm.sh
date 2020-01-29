fdd09816c9f3e702873ccf9892f499ad338ae63f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-01-29 15:48:06 +0100

    Update qemu version

--- build-arm.sh
+++ build-arm.sh
@@ -7,7 +7,7 @@ docker run --rm --privileged multiarch/qemu-user-static:register --reset
 # get qemu-arm-static binary
 mkdir tmp
 pushd tmp && \
-curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz && \
+curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v4.2.0-2/qemu-arm-static.tar.gz && \
 tar xzf qemu-arm-static.tar.gz && \
 popd
 
